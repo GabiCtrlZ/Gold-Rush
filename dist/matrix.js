08b9e970ae9526b2a69aee9793942b07e2b274bd
Gabi B <Gab3009@Gmail.com>
2019-07-24 14:55:53 +0300

    started rendering

--- dist/matrix.js
+++ dist/matrix.js
@@ -53,12 +53,19 @@ class Matrix {
 class GoldRush extends Matrix {
     constructor(x, y) {
         super(x, y)
+        this.ranCoinGenerator(x, y)
         this.matrix[0][0] = '1'
         this.matrix[this.matrix.length - 1][this.matrix[0].length - 1] = '2'
+        this.score = {'1':0, '2':0}
     }
     ranCoinGenerator(x, y){
-        for (let i = 0; i < x; i++){
-            
+        for (let i = 0; i < x*1.5; i++){
+            this.matrix[Math.floor(Math.random()*x)][Math.floor(Math.random()*y)] = 'c'
+        }
+    }
+    appedScore(player, pos){
+        if (this.matrix[pos.x][pos.y] == 'c'){
+            this.score[player] += 10
         }
     }
     movePlayer(player, direction) {
@@ -69,24 +76,28 @@ class GoldRush extends Matrix {
             case 'left':
                 if (y != 0) {
                     this.matrix[x][y] = '.'
+                    this.appedScore(player, {x, y: y-1})
                     this.matrix[x][y - 1] = player
                 }
                 break;
             case 'right':
                 if (y != (this.matrix[0].length - 1)) {
                     this.matrix[x][y] = '.'
+                    this.appedScore(player, {x, y: y+1})
                     this.matrix[x][y + 1] = player
                 }
                 break;
             case 'up':
                 if (x != 0) {
                     this.matrix[x][y] = '.'
+                    this.appedScore(player, {x: x-1, y})
                     this.matrix[x - 1][y] = player
                 }
                 break;
             case 'down':
                 if (x != (this.matrix.length - 1)) {
                     this.matrix[x][y] = '.'
+                    this.appedScore(player, {x: x+1, y})
                     this.matrix[x + 1][y] = player
                 }
         }
@@ -94,3 +105,13 @@ class GoldRush extends Matrix {
 }
 
 const board = new GoldRush(5, 5)
+board.print()
+board.movePlayer(1, "down")
+board.movePlayer(1, "down")
+board.movePlayer(1, "down")
+board.movePlayer(1, "down")
+board.movePlayer(1, "right")
+board.movePlayer(1, "up")
+board.movePlayer(1, "up")
+board.movePlayer(1, "up")
+console.log(board.score)
---
+++ dist/renderer.js
@@ -0,0 +1,5 @@
+class Render{
+    renderBoard(board){
+        
+    }
+}
