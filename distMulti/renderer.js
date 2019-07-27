c890a194c85d65b90c500bfec1db4959c54de968
Gabi B <Gab3009@Gmail.com>
2019-07-27 19:40:59 +0300

    finished css for now

--- distMulti/renderer.js
+++ distMulti/renderer.js
@@ -10,5 +10,15 @@ class Render{
     renderEndGame(text){
         $('.end').remove()
         $('body').append(`<div class='end'>Game Over ${text}</div>`)
+        setInterval(function(){
+            const css = $('.end').css('visibility')
+            console.log(css)
+            if (css == 'hidden'){
+                $('.end').css('visibility', 'visible')
+            }
+            else{
+                $('.end').css('visibility', 'hidden')
+            }
+        }, 1200)
     }
 }
